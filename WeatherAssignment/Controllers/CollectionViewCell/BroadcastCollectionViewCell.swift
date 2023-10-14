bc05220d8431edbf5db1a2540dac514e2cc8726e
Ravneet <ravneetjandu0@gmail.com>
2023-10-14 22:59:58 +0530

    Completed Project for Assignment Submission

---
+++ WeatherAssignment/Controllers/CollectionViewCell/BroadcastCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BroadcastCollectionViewCell.swift
+//  WeatherAssignment
+//
+//  Created by Ravneet Singh on 14/10/23.
+//
+
+import UIKit
+
+class BroadcastCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
